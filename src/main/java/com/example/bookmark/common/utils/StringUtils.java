a424e63443ccbfd8f69fdc08ccdb090058c5c43d
ShervilG <wintergpt45@gmail.com>
2020-12-23 00:34:54 +0530

    fixed util

--- src/main/java/com/example/bookmark/common/utils/StringUtils.java
+++ src/main/java/com/example/bookmark/common/utils/StringUtils.java
@@ -3,6 +3,6 @@ package com.example.bookmark.common.utils;
 public class StringUtils {
 
   public static boolean isNullOrEmpty(String x) {
-    return (x == null && x.length() == 0);
+    return (x == null || x.length() == 0);
   }
 }
